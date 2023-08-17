dcec3edd465df6baeb2b484b4a7f80845c02c46f
Thomas Berckmoes <103941542+thmBerck@users.noreply.github.com>
2023-08-17 17:18:31 +0200

    Update README/ABOUTME laravel project

--- Project1-Laravel/Website-TM/resources/views/aboutus/index.blade.php
+++ Project1-Laravel/Website-TM/resources/views/aboutus/index.blade.php
@@ -30,23 +30,87 @@
             <a href="https://laravel.com/docs/">https://laravel.com/docs/</a>
         </li>
         <li class="mb-4">
-            <b>Laravel Documentation by Laravel</b>
+            <b>Stackoverflow call route from button click laravel</b>
             <p>I've used this source to refresh memory on how to call a route from a button element.</p>
             <a href="https://stackoverflow.com/questions/38709886/call-route-from-button-click-laravel">https://stackoverflow.com/questions/38709886/call-route-from-button-click-laravel</a>
         </li>
-        
-            https://www.w3schools.com/tags/tag_textarea.asp
-            https://www.digitalocean.com/community/tutorials/simple-laravel-crud-with-resource-controllers
-            https://www.w3schools.com/css/css_dropdowns.asp
-            https://www.w3schools.com/tags/att_select_multiple.asp
-            https://www.honeybadger.io/blog/user-roles-permissions-in-laravel/
-            https://codeanddeploy.com/blog/laravel/laravel-8-user-roles-and-permissions-step-by-step-tutorial#PYl6F4poc6E3wnhNDaSmqNYXZ
-            https://spatie.be/docs/laravel-permission/v5/basic-usage/new-app
-            https://stackoverflow.com/questions/65551836/laravel-breeze-and-spatie-permissions-set-role-while-user-registered
-            https://cerwyn.medium.com/laravel-one-to-many-relationship-6474d29cfd87
-            https://www.stackhawk.com/blog/laravel-xss/
-            https://github.com/protonemedia/laravel-xss-protection
-            https://madewithlaravel.com/laravel-xss-protection
+        <li class="mb-4">
+            <b>W3schools.com Textareas</b>
+            <p>I've used this source to refresh memory on textareas.</p>
+            <a href="https://www.w3schools.com/tags/tag_textarea.asp">https://www.w3schools.com/tags/tag_textarea.asp</a>
+        </li>
+        <li class="mb-4">
+            <b>Laravel resource controllers by digitalocean</b>
+            <p>Looking up how to make resource controllers and how they are structured and how to automatically create them.</p>
+            <a href="https://www.digitalocean.com/community/tutorials/simple-laravel-crud-with-resource-controllers">https://www.digitalocean.com/community/tutorials/simple-laravel-crud-with-resource-controllers</a>
+        </li>
+        <li class="mb-4">
+            <b>W3schools css dropdowns</b>
+            <p>Looking up how to make css dropdowns, as I had forgotten how to.</p>
+            <a href="https://www.w3schools.com/css/css_dropdowns.asp">https://www.w3schools.com/css/css_dropdowns.asp</a>
+        </li>
+        <li class="mb-4">
+            <b>W3schools HTML select & option</b>
+            <p>More research on how to make dropdowns.</p>
+            <a href="https://www.w3schools.com/tags/att_select_multiple.asp">https://www.w3schools.com/tags/att_select_multiple.asp</a>
+        </li>
+        <li class="mb-4">
+            <b>User roles & permissions with Spatie's package by Honeybadger.</b>
+            <p>This website gave me an introduction on how powerful Spatie's package can be. I used it for my roles (admin/owner).</p>
+            <a href="https://www.honeybadger.io/blog/user-roles-permissions-in-laravel/">https://www.honeybadger.io/blog/user-roles-permissions-in-laravel/</a>
+        </li>
+        <li class="mb-4">
+            <b>User roles & permissions by Spatie by CodeAndDeploy.</b>
+            <p>This gave me a more detailed explanation on how to use it.</p>
+            <a href="https://codeanddeploy.com/blog/laravel/laravel-8-user-roles-and-permissions-step-by-step-tutorial#PYl6F4poc6E3wnhNDaSmqNYXZ">https://codeanddeploy.com/blog/laravel/laravel-8-user-roles-and-permissions-step-by-step-tutorial#PYl6F4poc6E3wnhNDaSmqNYXZ</a>
+        </li>
+        <li class="mb-4">
+            <b>User roles & permissions documentation by Spatie</b>
+            <p>This gave me an even more detailed explanation, certainly the example app tutorial, which is a very good tutorial to get in it.</p>
+            <a href="https://spatie.be/docs/laravel-permission/v5/basic-usage/new-app">https://spatie.be/docs/laravel-permission/v5/basic-usage/new-app</a>
+        </li>
+        <li class="mb-4">
+            <b>Stackoverflow post about laravel breeze and spatie permissions together.</b>
+            <p>Stackoverflow post that helped me to get my way in mixing Laravel Breeze with Spatie Roles & Permissions.</p>
+            <a href="https://stackoverflow.com/questions/65551836/laravel-breeze-and-spatie-permissions-set-role-while-user-registered">https://stackoverflow.com/questions/65551836/laravel-breeze-and-spatie-permissions-set-role-while-user-registered</a>
+        </li>
+        <li class="mb-4">
+            <b>One to many relationship by Cerwyn Cahyono</b>
+            <p>Medium blog that helped me make my one to many relationships in my application.</p>
+            <a href="https://cerwyn.medium.com/laravel-one-to-many-relationship-6474d29cfd87">https://cerwyn.medium.com/laravel-one-to-many-relationship-6474d29cfd87</a>
+        </li>
+        <li class="mb-4">
+            <b>Laravel XSS protection by Stackhawk</b>
+            <p>This is the blog that helped me protecting my application against XSS attacks. Together with the other ones.</p>
+            <a href="https://www.stackhawk.com/blog/laravel-xss/">https://www.stackhawk.com/blog/laravel-xss/</a>
+        </li>
+        <li class="mb-4">
+            <b>Laravel XSS protection by Stackhawk</b>
+            <p>This is the blog that helped me protecting my application against XSS attacks. Together with the other ones, i've successfully made a middleware that protects against XSS.</p>
+            <a href="https://www.stackhawk.com/blog/laravel-xss/">https://www.stackhawk.com/blog/laravel-xss/</a>
+        </li>
+        <li class="mb-4">
+            <b>Laravel XSS protection by madewithlaravel.</b>
+            <p>This is the blog that helped me protecting my application against XSS attacks. Together with the other ones, i've successfully made a middleware that protects against XSS.</p>
+            <a href="https://madewithlaravel.com/laravel-xss-protection">https://madewithlaravel.com/laravel-xss-protection</a>
+        </li>
+        <li class="mb-4">
+            <b>Laravel avatar uploading by laraveltuts.</b>
+            <p>This is the blog that helped me make the avatar functionality in my Laravel application.</p>
+            <a href="https://medium.com/@laraveltuts/how-to-upload-avatar-in-laravel-a-step-by-step-guide-77557ce2a45">https://medium.com/@laraveltuts/how-to-upload-avatar-in-laravel-a-step-by-step-guide-77557ce2a45</a>
+        </li>
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
     </ul>
 </div>
 @endsection
